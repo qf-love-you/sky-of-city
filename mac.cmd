604c38080e18b78039d98215cedcbd8b695adeb4
qf-love-you <139869077+qf-love-you@users.noreply.github.com>
2023-10-28 16:41:36 +0800

    Delete mac.cmd

--- mac.cmd
+++
@@ -1,7 +0,0 @@
-$ xcode-select --install
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-$ pip --version
-$ brew install hg sdl sdl_image sdl_ttf
-$ brew install sdl_mixer portmidi
-$ pip install --user hg+http://bitbucket.org/pygame/pygame
-brew install tcl-tk
